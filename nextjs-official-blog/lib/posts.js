b70c13bb8e5d3775be0ba0acbaa8e970ed43499a
lesenelir <miaomiaobabyzy@gmail.com>
2022-09-16 11:58:59 +0800

    feat: dynamic routes

--- nextjs-official-blog/lib/posts.js
+++ nextjs-official-blog/lib/posts.js
@@ -1,11 +1,12 @@
 import fs from 'node:fs'
 import path from 'node:path'
-// import matter from 'gray-matter'
 import matter from 'gray-matter'
+import { remark } from 'remark'
+import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-export function getSortedPostsData() {
+function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
   const allPostsData = fileNames.map((fileName) => {
@@ -26,7 +27,7 @@ export function getSortedPostsData() {
     };
   });
   // Sort posts by date
-  return allPostsData.sort(({ date: a }, { date: b }) => {
+  return allPostsData.sort(({date: a}, {date: b}) => {
     if (a < b) {
       return 1;
     } else if (a > b) {
@@ -35,4 +36,56 @@ export function getSortedPostsData() {
       return 0;
     }
   });
+}
+
+function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ''),
+      },
+    };
+  });
+}
+
+async function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents)
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+      .use(html)
+      .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  // Combine the data with the id and contentHtml
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data
+  }
+}
+
+
+export {
+  getSortedPostsData,
+  getAllPostIds,
+  getPostData
 }
--- nextjs-official-blog/pages/index.js
+++ nextjs-official-blog/pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link"
 import Head from "next/head"
 
 import Layout, {siteTitle} from "../components/layout"
+import Date from '../components/date'
 import {getSortedPostsData} from "../lib/posts"
 import utilStyles from '../styles/utils.module.css'
 
@@ -9,36 +10,38 @@ export default function Home(props) {
   const {allPostsData} = props
 
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
 
-      <h1>Index Pages</h1>
+        <h1>Index Pages</h1>
 
-      <section className={utilStyles.headingMd}>
-        <p>Hi, I am Lesenelir. You can visit my <a href="https://github.com/lesenelir">Github</a></p>
-      </section>
-      <h4>
-        Change <Link href="/posts/first-post">this page !</Link>
-      </h4>
+        <section className={utilStyles.headingMd}>
+          <p>Hi, I am Lesenelir. You can visit my <a href="https://github.com/lesenelir">Github</a></p>
+        </section>
+        <h4>
+          Change <Link href="/posts/first-post">this page !</Link>
+        </h4>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-                {title}
-                <br />
-                {id}
-                <br />
-                {date}
-              </li>
-          ))}
-        </ul>
-      </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({id, date, title}) => (
+                <li className={utilStyles.listItem} key={id}>
+                  <Link href={`/posts/${id}`}>
+                    <a>{title}</a>
+                  </Link>
+                  <br/>
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={date}/>
+                  </small>
+                </li>
+            ))}
+          </ul>
+        </section>
 
-    </Layout>
+      </Layout>
   )
 }
 
